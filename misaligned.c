e0098f4f1dcc75b110062dcde66a57ebe7308c54
MatheswaranPalanisamy <mathespbe@gmail.com>
2022-11-25 17:46:17 +0530

    Update misaligned.c to split the testable funcion for color pair and add test case for failure conditions

--- misaligned.c
+++ misaligned.c
@@ -1,21 +1,53 @@
 #include <stdio.h>
 #include <assert.h>
+#include <string.h> 
+
+const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
+const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+
+typedef struct{
+    const char *majorColor;
+    const char *minorColor;
+}colorPair;
+
+colorPair getColorCode(int i, int j){
+    colorPair colorPair;
+    colorPair.majorColor = majorColor[i];
+    colorPair.minorColor = minorColor[i];
+    return colorPair;
+}
 
 int printColorMap() {
-    const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
-    const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
     int i = 0, j = 0;
+    colorPair colorPair;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-            printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]);
+            colorPair = getColorCode(i, j);
+            printf("%d | %s | %s\n", i * 5 + j, colorPair.majorColor, colorPair.minorColor);
         }
     }
     return i * j;
 }
 
-int main() {
+int main(){
+    colorPair colorPair;
     int result = printColorMap();
     assert(result == 25);
+    printf("%d pairs printed successfully\n", result);
+    printf("Now lets test the validity of individula pairs\n");
+
+    colorPair = getColorCode(0, 0);
+    assert(strcmp(colorPair.majorColor, majorColor[0]) == 0);
+    assert(strcmp(colorPair.minorColor, minorColor[0]) == 0);
+
+    colorPair = getColorCode(0, 1);
+    assert(strcmp(colorPair.majorColor, majorColor[0]) == 0);
+    assert(strcmp(colorPair.minorColor, minorColor[1]) == 0);
+
+    colorPair = getColorCode(0, 2);
+    assert(strcmp(colorPair.majorColor, majorColor[0]) == 0);
+    assert(strcmp(colorPair.minorColor, minorColor[2]) == 0);
+
     printf("All is well (maybe!)\n");
     return 0;
 }
