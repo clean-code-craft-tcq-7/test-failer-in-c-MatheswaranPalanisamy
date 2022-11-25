0647306975694ab96a0afaf4bcab2d71cefd8755
MatheswaranPalanisamy <mathespbe@gmail.com>
2022-11-25 18:29:49 +0530

    Add product configuration where the production or unit test build can be configured

---
+++ product_config.h
@@ -0,0 +1,5 @@
+
+// Define UNIT_TEST incase of unit test environment. Remove in case of production build integration
+#define UNIT_TEST
+// Define PRODUCTION in case of production environment. Remove in case of unit test build
+// #define PRODUCTION
