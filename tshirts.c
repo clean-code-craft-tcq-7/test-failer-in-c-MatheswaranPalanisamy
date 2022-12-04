82049f292363f043d4d51b8b52060b1c2f0397f6
MatheswaranPalanisamy <mathespbe@gmail.com>
2022-12-04 14:50:44 +0530

    Update tshirts.c to fix bug in boundary condition for each size and add test cases for all the conditions

--- tshirts.c
+++ tshirts.c
@@ -3,9 +3,9 @@
 
 char size(int cms) {
     char sizeName = '\0';
-    if(cms < 38) {
+    if(cms <= 38) {
         sizeName = 'S';
-    } else if(cms > 38 && cms < 42) {
+    } else if(cms > 38 && cms <= 42) {
         sizeName = 'M';
     } else if(cms > 42) {
         sizeName = 'L';
@@ -14,22 +14,22 @@ char size(int cms) {
 }
 
 int main() {
-    // Test case 1: Validate the size 'S'
+    // Test case 1: Validate the size 'S' with in the range
     assert(size(37) == 'S');
     // Test case 2: Validate the size 'S' upper boundary
     assert(size(38) == 'S');
     // Test case 3: Validate the size 'M' lower boundary
-    assert(size(38) == 'M');
+    assert(size(39) == 'M');
     // Test case 4: Validate the size 'M'
     assert(size(40) == 'M');
     // Test case 5: Validate the size 'M' upper boundary
     assert(size(42) == 'M');
-    // Test case 6: Validate the size 'L' lower boundary
-    assert(size(42) == 'L');
-    // Test case 7: Validate the size 'L' range
+    // Test case 6: Validate the size 'L' Lower boundary
     assert(size(43) == 'L');
+    // Test case 7: Validate the size 'L' in the range
+    assert(size(44) == 'L');
 
     // Conclusion: Here the sizes 38 and 42 are not considered for any of the sizeName check
-    printf("All is well (maybe!)\n");
+    printf("All is well !!!\n");
     return 0;
 }
